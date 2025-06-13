dda9d886ba5250359c33597a71cb3ca8701fe308
jamileth-kumpanam <jamilethkumpanam@MacBook-Air-de-Jamileth.local>
2025-06-12 22:24:55 -0500

    Interfacez

---
+++ src/main/java/ec/edu/ups/CarritoCantidad.java
@@ -0,0 +1,108 @@
+package ec.edu.ups;
+
+import ec.edu.ups.modelo.Producto;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+public class CarritoCantidad extends JFrame {
+    private JTextField textField2;
+    private JTextField textField1;
+    private JButton limpiarButton;
+    private JButton guardarButton;
+    private JLabel Producto;
+    private JLabel Cantidad;
+    private JPanel panelPrincipal2;
+
+    public CarritoCantidad(){
+
+        setContentPane(panelPrincipal2);
+        setTitle("Datos del Producto");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 500);
+        //setResizable(false);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        //pack();
+
+        limpiarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                limpiarCampos();
+            }
+        });
+    }
+
+    public JTextField getTextField2() {
+        return textField2;
+    }
+
+    public void setTextField2(JTextField textField2) {
+        this.textField2 = textField2;
+    }
+
+    public JTextField getTextField1() {
+        return textField1;
+    }
+
+    public void setTextField1(JTextField textField1) {
+        this.textField1 = textField1;
+    }
+
+    public JButton getLimpiarButton() {
+        return limpiarButton;
+    }
+
+    public void setLimpiarButton(JButton limpiarButton) {
+        this.limpiarButton = limpiarButton;
+    }
+
+    public JButton getGuardarButton() {
+        return guardarButton;
+    }
+
+    public void setGuardarButton(JButton guardarButton) {
+        this.guardarButton = guardarButton;
+    }
+
+    public JLabel getProducto() {
+        return Producto;
+    }
+
+    public void setProducto(JLabel producto) {
+        Producto = producto;
+    }
+
+    public JLabel getCantidad() {
+        return Cantidad;
+    }
+
+    public void setCantidad(JLabel cantidad) {
+        Cantidad = cantidad;
+    }
+
+    public JPanel getPanelPrincipal2() {
+        return panelPrincipal2;
+    }
+
+    public void setPanelPrincipal2(JPanel panelPrincipal2) {
+        this.panelPrincipal2 = panelPrincipal2;
+    }
+
+    public void mostrarMensaje(String mensaje) {
+        JOptionPane.showMessageDialog(this, mensaje);
+    }
+
+    public void limpiarCampos() {
+        textField2.setText("");
+        textField1.setText("");
+    }
+
+    public void mostrarProductos(List<Producto> productos) {
+        for (Producto producto : productos) {
+            System.out.println(producto);
+        }
+    }
+}
--- src/main/java/ec/edu/ups/vista/Main.java
+++ src/main/java/ec/edu/ups/vista/Main.java
@@ -1,5 +1,6 @@
 package ec.edu.ups.vista;
 
+import ec.edu.ups.CarritoCantidad;
 import ec.edu.ups.controlador.ProductoController;
 import ec.edu.ups.dao.ProductoDAO;
 import ec.edu.ups.dao.impl.ProductoDAOMemoria;
@@ -12,8 +13,12 @@ public class Main {
             public void run() {
 
                 ProductoView productoView = new ProductoView();
+                productoView.setLocation(200, 250);
                 ProductoDAO productoDAO = new ProductoDAOMemoria();
                 new ProductoController(productoDAO, productoView);
+                CarritoCantidad carrito = new CarritoCantidad();
+                carrito.setLocation(900, 250 );
+                carrito.setVisible(true);
             }
         });
     }
